1c47d9df05895ee0362305c75f2054a6c5e49fd7
minj0i <h_hoh@naver.com>
2021-03-24 17:40:11 +0900

    env 설정

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,12 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDahk25dN0-4jyEpCAMma4PARK3uMn1Db4",
-    authDomain: "nwitter-c7c4e.firebaseapp.com",
-    projectId: "nwitter-c7c4e",
-    storageBucket: "nwitter-c7c4e.appspot.com",
-    messagingSenderId: "199935986727",
-    appId: "1:199935986727:web:3441586e6fd06736a51202"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
   export default firebase.initializeApp(firebaseConfig);
-  
