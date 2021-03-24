50a4e3b35cd5a500ead834e2e68a11418d275647
minj0i <h_hoh@naver.com>
2021-03-24 16:57:50 +0900

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDahk25dN0-4jyEpCAMma4PARK3uMn1Db4",
+    authDomain: "nwitter-c7c4e.firebaseapp.com",
+    projectId: "nwitter-c7c4e",
+    storageBucket: "nwitter-c7c4e.appspot.com",
+    messagingSenderId: "199935986727",
+    appId: "1:199935986727:web:3441586e6fd06736a51202"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
+  
