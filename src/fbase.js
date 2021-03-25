6250030398548604476d9c346a4ab0d6646c1e31
minj0i <h_hoh@naver.com>
2021-03-25 15:50:54 +0900

    #2.5 Social Login

--- src/fbase.js
+++ src/fbase.js
@@ -7,9 +7,11 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
-  appId: process.env.REACT_APP_APP_ID
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
